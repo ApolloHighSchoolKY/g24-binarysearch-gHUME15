3e07648033a5903bf59532d7e4df1a0019974d4d
gHUME15 <Grayson.hume@stu.daviess.kyschools.us>
2025-04-16 17:30:13 +0000

    Done

--- BinarySearch.java
+++ BinarySearch.java
@@ -4,6 +4,32 @@ public class BinarySearch
 {
 	public static int binarySearch(int[] ray, int item)
 	{
+		Arrays.sort(ray);
+		int bottom = 0;
+		int top = ray.length-1;
+		while( bottom <= top)
+		{
+			int avg = (bottom + top) / 2;
 
+			if (ray[avg] == item)
+				return avg;
+
+
+			if (ray[avg] > item)
+				top = avg - 1;
+
+
+			else
+				bottom = avg + 1;
+		}
+			return -1;
+		
+		
+		// to low then to high
+		//Arrays.sort(arrayName)
+		//Arrays.reverse(arreayName)
+		//When bottom is grater then the top stop
+		
 	}
+	
 }
